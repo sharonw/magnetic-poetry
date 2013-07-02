14260e42fe6fbd71292c4f8f8fc83472d32d505d
Sharon Wong <sharonwong@alumni.cmu.edu>
2013-07-01 23:49:05 -0700

    Improve regex for identifying character names
    e.g., Amber D -> Amber D'Alessio

--- poetry.py
+++ poetry.py
@@ -6,13 +6,20 @@ http://magneticpoetry.com/category/voices-word-magnets/
 """
 
 import re
+SRE_MATCH_TYPE = type(re.match("", ""))
 
 names = set()
 quotes = open('mean-girls.txt', 'r')
 
-regex = re.compile("^[A-Za-z. ]*")
+regex = re.compile("^[A-Za-z.' ]*:")
 for line in quotes:
     m = regex.match(line)
-    names.add(m.group())
+
+    # If we found a match, m is SRE_Match
+    # Otherwise, if there's no match, it's NoneType
+
+    if type(m) is SRE_MATCH_TYPE:
+        name = m.group()
+        names.add(name[:-1])
 
 print names
